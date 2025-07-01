38d615defe5c76da6dfbb9f82e2d0fac5c9e5673
Zach West <zwest2563@gmail.com>
2025-07-01 11:26:42 -0500

    Solution 2: Optimal/Expected Solution I viewed the suggested solution and rebuilt it. I did not like how the NeetCode solution provided tracked multiple indexes. I made my solution so that it instead revolved more around strings & string methods

--- Arrays-N-Hashing/Encode-Decode.py
+++ Arrays-N-Hashing/Encode-Decode.py
@@ -1,24 +1,34 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        '''
-        strs act
-
-
-
-        '''
         encodedString = ""
-        delimiter = "NeetCodeSolution" # length = 17
-        for strings in strs:
-            encodedString = encodedString + strings + delimiter
+        for string in strs:
+            stringLength = str(len(string))
+            delimiter = '#'
+            encodedString += delimiter + stringLength + delimiter + string
+
         return encodedString
 
 
     def decode(self, s: str) -> List[str]:
-        delimiter = "NeetCodeSolution"
-        if s == "":
-            return []
-        return s.split(delimiter)[:-1]  
-        
-        
+        decodedList = []
+        index = 0
+        delimiter = '#'
+        #   "#4#This#2#is#2#an#7#example"
+        while (index < len(s)):
+    
+            if (s[index] == delimiter): 
+                index += 1 #skip past first delimiter
+                
+                delimiter_index = s.find('#', index) #find the NEXT '#'
+                length = int(s[index: delimiter_index]) # converts the string between #'s to a number
+                index = delimiter_index + 1 # take one past 2nd '#' AKA beginning of word
 
+                word = ""
+                word = s[index:index + length] #word is grabbed by the delimiter data (this prevents confusion by #'s included in string itself)
+
+                decodedList.append(word)
+                index += length 
+            else:
+                index +=1
+        return decodedList
